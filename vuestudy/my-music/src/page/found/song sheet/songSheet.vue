22521454b6cc440f2e036c3e86361b07067e6e91
cnzhhx <33822028+cnzhhx@users.noreply.github.com>
2018-10-15 21:32:09 +0800

    QAQ

--- vuestudy/my-music/src/page/found/song sheet/songSheet.vue
+++ vuestudy/my-music/src/page/found/song sheet/songSheet.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="songSheet">
-    这里是歌单
+    <div class="songSheetBig">
+      <div></div>
+    </div>
   </div>
 </template>
 
@@ -8,10 +10,25 @@
 
 
   export default {
-
+    name: "songSheet"
   }
 </script>
 
 <style>
-
+  *{
+    font-size: 12px;
+    color: #333;
+    font-family: Arial, Helvetica, sans-serif;
+  }
+  .songSheet{
+    margin-top: 1px;
+  }
+  .songSheetBig{
+    width: 980px;
+    min-height: 700px;
+    margin: 0 auto;
+    background-color: #fff;
+    border-left: 1px solid #d3d3d3;
+    border-right: 1px solid #d3d3d3;
+  }
 </style>
